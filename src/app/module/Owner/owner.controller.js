28a810230ab1bd32477d5526c2274db132f48f21
mohammad-ishtiaque <mohammad.ishtiaque833@gmail.com>
2025-07-17 16:03:33 +0600

    slight changes

--- src/app/module/Owner/owner.controller.js
+++ src/app/module/Owner/owner.controller.js
@@ -24,50 +24,49 @@ exports.getOwnerDetails = asyncHnadler(async (req, res, next) => {
 
 exports.updateOwnerDetails = asyncHnadler(async (req, res, next) => {
 
-    const { id } = req.owner.id;
-  
-    const owner = await Owner.findById(id);
-    if (!owner) {
-      return next(new ApiError('Owner not found', 404));
-    }
-    owner.name = req.body.name;
-    owner.email = req.body.email;
-    owner.phone = req.body.phone;
-    owner.address = req.body.address;
-    owner.city = req.body.city;
-    owner.state = req.body.state;
-    owner.zipCode = req.body.zipCode;
-    owner.country = req.body.country;
-    if (req.file) {
-      owner.ownerPic = req.file.path; // upload the new image
-      await deleteFile(owner.ownerPic); // delete the old image 
-    } 
-    if (req.body.password) {
-      owner.password = req.body.password;
-    }
-    await owner.save();
-    res.status(200).json({
-      success: true,
-      message: 'Owner updated successfully',
-      owner
-    });
-  });
-  
-exports.deleteOwner = asyncHnadler(async (req, res, next) => {
-    const { id } = req.owner.id;
-    const owner = await Owner.findByIdAndDelete(id);
-    const businesses = await Business.find({ ownerId: id });
-    for (const business of businesses) {
-      await Business.findByIdAndDelete(business._id);
-    }
-    if (!owner) {
-      return next(new ApiError('Owner not found', 404));
-    }
-    res.status(200).json({
-      success: true,
-      message: 'Owner deleted successfully',
-      owner
-    });
-  });
+  const id  = req.owner.id || req.owner._id;
 
-  
+  const owner = await Owner.findById(id);
+  if (!owner) {
+    return next(new ApiError('Owner not found', 404));
+  }
+  owner.name = req.body.name;
+  owner.email = req.body.email;
+  owner.phone = req.body.phone;
+  owner.address = req.body.address;
+  owner.city = req.body.city;
+  owner.state = req.body.state;
+  owner.zipCode = req.body.zipCode;
+  owner.country = req.body.country;
+  if (req.file) {
+    owner.ownerPic = req.file.path; // upload the new image
+    await deleteFile(owner.ownerPic); // delete the old image 
+  }
+  if (req.body.password) {
+    owner.password = req.body.password;
+  }
+  await owner.save();
+  res.status(200).json({
+    success: true,
+    message: 'Owner updated successfully',
+    owner
+  });
+});
+
+exports.deleteOwner = asyncHnadler(async (req, res, next) => {
+  const id = req.owner.id || req.owner._id;
+  const owner = await Owner.findByIdAndDelete(id);
+  const businesses = await Business.find({ ownerId: id });
+  for (const business of businesses) {
+    await Business.findByIdAndDelete(business._id);
+  }
+  if (!owner) {
+    return next(new ApiError('Owner not found', 404));
+  }
+  res.status(200).json({
+    success: true,
+    message: 'Owner deleted successfully',
+    owner
+  });
+});
+
