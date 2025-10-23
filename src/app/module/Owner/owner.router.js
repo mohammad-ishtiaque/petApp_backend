b7e2a72a33c6f76c583b5a6923100dfd67f133a2
mohammad-ishtiaque <mohammad.ishtiaque833@gmail.com>
2025-10-23 12:31:00 +0600

    delete account

--- src/app/module/Owner/owner.router.js
+++ src/app/module/Owner/owner.router.js
@@ -6,7 +6,7 @@ const upload = require('../../../utils/upload');
 
 router.get('/get-owner-details', authenticateOwner, getOwnerDetails);
 router.put('/update-owner-details', authenticateOwner, upload.single('profilePic'), updateOwnerProfile);
-router.delete('/delete-owner', authenticateOwner, deleteOwner);
+// router.delete('/delete-owner', authenticateOwner, deleteOwner);
 router.get('/get-owner-businesses', authenticateOwner, getOwnerBusinesses);
 router.get('/get-bookings-by-owner', authenticateOwner, getAllBookingsByOwner);
 router.put('/update-booking-status/:id', authenticateOwner, updateBookingStatus);
